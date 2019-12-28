7c564ab38ae1b595ae1afabf5cbef65405f9489f
TanJiaJun <1120571286@qq.com>
2019-12-28 15:22:38 +0800

    1、升级Kotlin协程版本。 2、添加androidx.lifecycle:lifecycle-viewmodel-ktx:2.2.0-rc03依赖项。 3、优化代码。

--- app/src/main/java/com/tanjiajun/livedatademo/fragment/SecondFragment.kt
+++ app/src/main/java/com/tanjiajun/livedatademo/fragment/SecondFragment.kt
@@ -10,9 +10,6 @@ import androidx.lifecycle.ViewModelProviders
 import com.tanjiajun.livedatademo.R
 import com.tanjiajun.livedatademo.databinding.FragmentSecondBinding
 import com.tanjiajun.livedatademo.viewmodel.MainViewModel
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withContext
 
 /**
  * Created by TanJiaJun on 2019-12-22.
@@ -43,15 +40,11 @@ class SecondFragment : Fragment(), SecondHandlers {
             .root
 
     override fun onChangeFirstContentClick(view: View) {
-        viewModel?.firstContent?.value = getString(R.string.first_content_changed)
+        viewModel?.changeFirstContent(getString(R.string.first_content_changed))
     }
 
     override fun onChangeSecondContentClick(view: View) {
-        runBlocking {
-            withContext(Dispatchers.Default) {
-                viewModel?.secondContent?.postValue(getString(R.string.second_content_changed))
-            }
-        }
+        viewModel?.changeSecondContent(getString(R.string.second_content_changed))
     }
 
 }
--- app/src/main/java/com/tanjiajun/livedatademo/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/tanjiajun/livedatademo/viewmodel/MainViewModel.kt
@@ -1,19 +1,37 @@
 package com.tanjiajun.livedatademo.viewmodel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 /**
  * Created by TanJiaJun on 2019-12-22.
  */
 class MainViewModel : ViewModel() {
 
-    val firstContent: MutableLiveData<String> by lazy {
-        MutableLiveData<String>().apply { value = "第一个文本" }
+    private val _firstContent = MutableLiveData<String>().apply {
+        value = "第一个文本"
+    }
+    val firstContent: LiveData<String> = _firstContent
+
+    private val _secondContent = MutableLiveData<String>().apply {
+        value = "第二个文本"
+    }
+    val secondContent: LiveData<String> = _secondContent
+
+    fun changeFirstContent(text: String) {
+        _firstContent.value = text
     }
 
-    val secondContent: MutableLiveData<String> by lazy {
-        MutableLiveData<String>().apply { value = "第二个文本" }
-    }
+    fun changeSecondContent(text: String) =
+        viewModelScope.launch {
+            withContext(Dispatchers.Default) {
+                _secondContent.postValue(text)
+            }
+        }
 
 }
